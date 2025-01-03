36bf2aafdf60dc5966abc3f66b35aee60f2ae61d
dharanidharan85 <dharanidharansakthivel85@gmail.com>
2025-01-03 18:14:33 +0530

    first commits

---
+++ food-delivery/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+         <h2>Welcome To our Restaurant</h2>
+            <button>View menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
